454f8df717c31af5136b265c1f4bceef4b90baa9
Ali <38077025+ATahma@users.noreply.github.com>
2025-11-25 21:48:18 -0500

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atahma.github.io',
-  base: '/plur',
+  site: 'https://www.canopee.diy',
+  // base: '/plur',
   // Only use base path in production (GitHub Pages)
 });
